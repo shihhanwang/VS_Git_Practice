2a5ab162776ecc346ff6d38b1c840affa4789832
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-09 10:46:32 +0100

    initial check-in of version control practice

---
+++ VS_GIt_practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS_GIt_practice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
